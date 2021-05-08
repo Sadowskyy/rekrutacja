39bc48a7b10e9a13b6e25f4048323373832112a3
Wiktor <wiktorsadowski.jd@gmail.com>
2021-05-08 20:42:52 +0200

    Init

---
+++ src/Controller/MainController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class MainController extends AbstractController
+{
+    const URL = 'https://reqres.in/api/users';
+
+    public function __construct(private HttpClientInterface $httpClient)
+    {
+    }
+
+    #[Route('/users', name: 'create_user')]
+    public function create(Request $request): Response
+    {
+        $response = $this->httpClient->request(
+            'POST',
+            self::URL,
+            ['body' => [
+                'name' => $request->request->get('name'),
+                'job' => $request->request->get('job')
+            ]]
+        )->getContent();
+
+        return new Response($response);
+    }
+
+    #[Route('/users/{id}', name: 'get_user')]
+    public function get($id): Response
+    {
+        $response = $this->httpClient->request(
+            'GET',
+            self::URL . $id
+        )->getContent();
+
+        return new Response($response);
+    }
+
+
+}
